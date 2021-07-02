1d6a31aa9b71e894e0b5f2e99921951243b5557e
Josh Payne <paynejoshua1@gmail.com>
2021-07-02 08:40:29 -0700

    headed custom header logo to home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import NavItems from "../components/NavItems";
 import SocialMedia from "../components/SocialMedia";
+import HeaderLogo from "../assets/HeaderLogo.png";
 
 
 function Home(){
@@ -9,8 +10,13 @@ function Home(){
         <>
         
                 
-            <Jumbotron fluid className="d-flex justify-content-center customJumbotron">
-                <h1 className="glassEffect" style={{fontSize: "5rem"}}>Unbounding Creative</h1>
+            <Jumbotron fluid className="customJumbotron">
+                <div className="glassEffect d-flex justify-content-center">
+
+                    <img alt="Header Logo" src={HeaderLogo} style={{maxWidth: "50%", height: "auto"}}/>
+
+                </div>
+
             </Jumbotron>
 
             <NavItems index={0}/>
