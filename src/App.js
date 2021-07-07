125f7d26a9b623599c8c2e4a3d1b27792c9b58f9
Josh Payne <paynejoshua1@gmail.com>
2021-07-07 10:34:24 -0700

    working on getting routes to work on github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Switch from "react-router-dom/Switch"
 import PageNav from "./components/PageNavigation";
 
 
